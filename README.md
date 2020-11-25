e7fa2e4126264704d83345a64db1ced264b35adb
ii <qb2main@gmail.com>
2020-11-25 14:35:10 +0200

    Save employee and add readme information

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ My studint of html and css
 Софія Брощак
 
 гр. КН-21
+
+
+В лабораторній роботі по створенню списку працівників було використано JSON Server
+http://zetcode.com/javascript/jsonserver/
--- js/employees.js
+++ js/employees.js
@@ -23,147 +23,111 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
-
+/**
+ * Function for getting all employees from db.json
+ */
 getUsersList();
 async function getUsersList() {
     var url = 'http://localhost:3000/employees';
-
     let response = await fetch(url);
 
     var list_html = '';
     if (response.ok) {
         var employees_object = await response.json();
-        // alert(json);
-        // alert(json[0].name);
+        // get element where will be placed employees list in html
         var employee_list_element = document.getElementById("employee-list");
         Object.entries(employees_object).forEach(function(element) {
             if (element.length > 1) {
                 Object.entries(element).forEach(function(user) {
                     if (user[1].id !=undefined) {
+                        // creating employees list
                         let user_id = user[1].id;
                         let user_name = user[1].name;
-                        list_html += '<li id="' + user_id + '">' + user_name + '</li>';
+                        list_html += '<li id="' + user_id + '" onclick="getUser(' + user_id  + ')" class="employee_list" >' + user_name + '</li>';
                     }
                 });
             }
         })
         employee_list_element.innerHTML = list_html;
     } else {
-        alert("Помилка HTTP: " + response.status);
+        alert("Error HTTP: " + response.status);
     }
 }
 
-getUser(3);
+/**
+ * Function for getting one employee info from db.json
+ * @param id
+ * @returns {Promise<void>}
+ */
 async function getUser(id) {
     var url = 'http://localhost:3000/employees/' + id + '/';
-    // var url = 'http://localhost:3000/employees/2/';
-
     let response = await fetch(url);
 
     var info_html = '';
     if (response.ok) {
         var employee_object = await response.json();
+        // get element where will be placed employee info in html
         var employee_info_element = document.getElementById("employee-info");
         if (employee_object.id != undefined) {
             let user_id = employee_object.id;
             let user_name = employee_object.name;
             let user_text = employee_object.text;
+            // creating employee info
             info_html += '<div> id: ' + user_id + '<br> name: ' + user_name + '<br> text: ' + user_text + '</div>';
         }
         employee_info_element.innerHTML = info_html;
     } else {
-        alert("Помилка HTTP: " + response.status);
+        alert("Error HTTP: " + response.status);
     }
+
+    var list_elements = document.getElementsByClassName('employee_list');
+    for(var i = 0; i < list_elements.length; i++){
+        list_elements[i].style.color = "black";
+    }
+    document.getElementById(id).style.color = 'red';
+
 }
 
+/**
+ * Function for saving employee in db.json
+ * @param e
+ * @returns {Promise<void>}
+ */
+async function createUser(e) {
+    // no run submit when press form submit button
+    e.preventDefault();
 
+    //get entered data from form
+    let name = document.getElementById('form_name').value;
+    let text = document.getElementById('form_text').value;
 
+    var url = 'http://localhost:3000/employees/';
+    let new_user = {
+        name: name,
+        text: text,
+        image: "image url"
+    };
 
+    // make request taht will be save employee data in db.jsom
+    let response = await fetch(url,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(new_user)
+    });
 
-
-
-
-
-
-
-
-
-const refery_click_button = document.getElementById('restart-btn');
-const player_1_click_button = document.getElementById('character-btn-kick');
-const player_2_click_button = document.getElementById('enemy-btn-kick');
-
-const refery = {
-    element: document.getElementById('refery'),
-    name: 'Refery',
-    winner_name: document.getElementById('winner_name'),
-}
-
-const player_1 = {
-    name: 'Pikachu',
-    max_health: 100,
-    current_health: 100,
-    health: document.getElementById('health-character'),
-    progress_bar: document.getElementById('progressbar-character')
-}
-
-const player_2 = {
-    name: 'Charmander',
-    max_health: 100,
-    current_health: 100,
-    health: document.getElementById('health-enemy'),
-    progress_bar: document.getElementById('progressbar-enemy')
-}
-
-refery_click_button.addEventListener('click', () => {
-    location.reload();
-})
-
-player_1_click_button.addEventListener('click', () => {
-    changeHp(random(15), player_1)
-})
-
-player_2_click_button.addEventListener('click', () => {
-    changeHp(random(15), player_2)
-})
-
-const init = () => {
-    refery.element.style.display = 'none';
-    renderHp(player_1)
-    renderHp(player_2)
-    random(15)
-}
-
-const renderHp = person => {
-    renderHpLife(person)
-    renderProgressbarHp(person)
-}
-
-const renderHpLife = person => {
-    person.health.innerText = person.current_health + ' / ' + person.max_health
-}
-
-const renderProgressbarHp = person => {
-    person.progress_bar.style.width = person.current_health + '%'
-}
-
-const changeHp = (count, person) => {
-    if (person.current_health < count) {
-        person.current_health = 0
-        refery.element.style.display = 'block';
-        let winner = 'No winner';
-        if (person.name == 'Pikachu' ) winner = 'Charmander';
-        else winner = 'Pikachu';
-        refery.winner_name.innerHTML = 'WINNER <br>' + winner;
-        player_1_click_button.setAttribute("disabled", true);
-        player_2_click_button.setAttribute("disabled", true);
+    if (response.ok) {
+        // show message that user was saved to db.json
+        alert('User created!');
+        // close modal window with form
+        modal.style.display = "none";
+        // refresh list of employees
+        getUsersList();
     } else {
-        person.current_health -= count
+        alert("Error HTTP: " + response.status);
     }
-    renderHp(person)
+
+
 }
 
-const random = num => {
-    return Math.ceil(Math.random() * num)
-}
-
-init()
